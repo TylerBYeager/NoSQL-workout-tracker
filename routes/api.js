2f56a4a0334df4ecddb64576a774336f46a10802
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-14 16:45:15 -0800

    added get all workouts route

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,16 @@
 const router = require("express").Router();
 const path = require("path");
 
+router.get("/api/workouts", async (req, res) => {
+    try {
+        const data = await db.Workout.find({});
+        res.status(200).json(data);
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+});
+
+
+
 module.exports = router;
