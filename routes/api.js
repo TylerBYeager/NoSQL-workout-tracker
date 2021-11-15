e13bd035ee900edee3dd1aeb234be3c8b2c54305
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-14 21:26:31 -0800

    add workout put route

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const path = require("path");
+const db = require("../models");
 
 router.get("/api/workouts", async (req, res) => {
     try {
@@ -15,6 +16,21 @@ router.get("/api/workouts/range", async ({}, res) => {
     try {
         const data = await db.Workout.find({})
         res.status(200).json(data);
+    } catch (err) {
+        console.log("error", err);
+        res.status(404).json(err);
+    }
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+    try {
+        const data = await db.Workout.findByIdAndUpdate(req.params.id, 
+            {$push: {exercises: req.body}});
+            if (!data) {
+                res.status(404).json("Exercise doesn't exist");
+            } else {
+                res.status(200).json(data);
+            }
     } catch (err) {
         console.log("error", err);
         res.status(404).json(err);
